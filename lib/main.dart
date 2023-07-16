eaee8b87a170c24b5d438f776ed2863e534a431b
jkoob04 <134137248+jkoob04@users.noreply.github.com>
2023-07-16 14:44:23 +0330

    remove comment in main

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     currentweatherfuture = sendrequstcurrentweather();
   }
